e419187944b1a9eb357190194505be2b43ec82f6
causingchaos <justin.db83@gmail.com>
2017-04-04 06:27:08 -0600

    adding inital irc bot code

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 This is a simple IRC bot written in Python3
+
+Usage: bot <hostname> <port> <channel> <secret-phrase>
+
+Example:
+python3 bot.py chat.freenode.net 6667 Windows95 iamhere
+
+Bot will join the server chat.freenode.net on port 6667
+and will wait for the controller to say the secret phrase to enable control mode
--- bot.py
+++ bot.py
@@ -2,42 +2,64 @@
 
 import sys
 import socket
+import random
+import string
 
 class PyBot:
   
   #PyBot object constructor 
-  def __init__(self,host,port,channel,secret):
+  def __init__(self,host,port,channel,secret,s):
 
+    #Create PyBot instanace variables
     self.host = host
     self.port = port
     self.channel = channel
     self.secret = secret
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host,int(port)))
-    user = 'AndrewBot'
-    try:
-      s.send(bytes("NICK "+user+"\n","UTF-8"))
-      s.send(bytes("USER "+user+" "+user+" "+user+" "+user+"\n","UTF-8"))
-      print("DEUBG --> Successfully joined IRC server "+ host)
-    except:
-      print("DEBUG -> unable to join server")
-    while True:
-      #Join the Channel
-      s.send(bytes("JOIN "+channel+"\n","UTF-8"))
-      data = ""
-      while data.find("End of /NAMES list.") == -1:
-        data = s.recv(4096).decode("UTF-8")
-        data = data.strip('\n\r')
-      print("DEUBG -> Successfully joined " + channel)
-    
-  #function: changeChannel( arg1: new channel )
-  #description: quits current channel and joins another specified channel
-  def changeChannel(self,newChannel):
-    print("Changing channel")     
+    self.nick = self.generateRandomName()
+    self.controlMode = 0 #default 0 = bot not being controller
 
-  def getChannel(self):
-    return self.channel
-  
+    try:
+      self.s = s.connect((host,int(port)))
+      s.send(bytes("NICK " + self.nick + "\n", "UTF-8"))
+      s.send(bytes("USER " +self.nick+" "+self.nick+" "+self.nick+ " " + self.nick+ "\n","UTF-8"))
+    except:
+      print("unable to connect to irc server")
+    s.send(bytes("JOIN " + channel + "\n", "UTF-8")) #Join channel
+
+  def listen(self):
+    while True:
+      data = ""
+      data = s.recv(4096).decode("UTF-8")
+      data = data.strip("\n\r")
+      print("DEBUG --> data recieved: " + data)
+
+      if data.find("PING :") != -1:  #Respond to server pings (keep alive)
+        s.send(bytes("PONG :pingis\n","UTF-8"))
+
+      elif data.find("PRIVMSG") != -1:
+        temp = data.split("!")
+        user = temp[0][1:] #extract and trim user from temp
+        temp = data.split("PRIVMSG")
+        temp2 = temp[1].split(":")
+        message = temp2[1]
+        print("DEBUG --> " + user + " Says: " + message)
+        self.examinePrivmsg(user,message)
+
+  def examinePrivmsg(self,user,message):
+    if message == secret:
+      print("DEBUG --> Controller said the secret! Control Mode enabled, read to troll and annoy")
+      self.control = 1
+
+  def changeChannel(self,newChannel):
+    print("DEBUG --> Changing channel")     
+
+  def generateRandomName(self):
+    randomName = "Andrew" + random.choice(string.digits)
+    randomName += random.choice(string.digits)
+    randomName += random.choice(string.digits)
+    randomName += random.choice(string.digits)
+    return randomName
+
 #Main
 if __name__ == '__main__':
 
@@ -46,9 +68,23 @@ if __name__ == '__main__':
     port = sys.argv[2]
     channel = "#"+sys.argv[3]
     secret = sys.argv[4]
-    bot = PyBot(host,port,channel,secret)
   else:
     print("Incorrect number of arguments")
     sys.exit("Usage: bot <hostname> <port> <channel> <secret-phrase>")
-  bot = PyBot(host,port,channel,secret)
-  #bot.changeChannel("#godlovesusall")
+
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  bot = PyBot(host,port,channel,secret,s) #Create an instance of the bot 
+  bot.listen() #listen to channel for incoming data over socket
+  
+
+
+
+
+
+
+
+
+
+
+
+
